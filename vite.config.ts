937584032a8c8b9a7c50b0bb8b9a4d1367bea018
Lxg1c <teran_2005@mail.ru>
2025-05-23 13:55:12 +0300

    add alias and installed tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// Эмуляция __dirname для ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss(),],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+});
